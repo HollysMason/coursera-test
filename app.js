71f9f0151d3b98788f7f4b89152da7bf705d9b13
HollysMason <ibondar204@gmail.com>
2016-10-19 15:33:00 +0300

    First Task. Weekend one

--- app.js
+++ app.js
@@ -1,39 +1,7 @@
-// (function () {
-//   'use strict';
-//
-//
-//   // .controller('DIController', 'MsgController', ['$scope', '$filter', DIController])
-//   //
-//   // function DIController($scope, $filter) {
-//   //   $scope.name = "Vlad";
-//   //
-//   //   $scope.upper = function () {
-//   //     var upCase = $filter('uppercase');
-//   //     $scope.name = upCase($scope.name);
-//   //   }
-//   // }
-//
-//   angular.module('DIApp', [])
-//   .controller("MsgController", MsgController)
-//
-//   MsgController.$inject = ['$scope'];
-//   function MsgController($scope) {
-//     $scope.name = "Vlad";
-//     $scope.picture = "wayne";
-//
-//     $scope.sayMessage = function () {
-//       return "Hello syka"
-//     };
-//     $scope.showWhoIsThis = function () {
-//       $scope.picture = "batman";
-//     }
-//
-//   }
-// })();
 (function () {
   'use sctrict';
 
-  angular.module('DIApp', [])
+  angular.module('FirstTask', [])
 
   .controller('CountFoodController', CountFoodController);
   CountFoodController.$inject = ['$scope'];
