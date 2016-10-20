07efb07817ee78c1887dfe6c31a9ad8762768fd2
HollysMason <ibondar204@gmail.com>
2016-10-20 13:59:52 +0300

    Unknown error trying to create custom filter

--- app.js
+++ app.js
@@ -1,28 +1,62 @@
+// (function () {
+//   'use sctrict';
+//
+//   angular.module('FirstTask', [])
+//
+//   .controller('CountFoodController', CountFoodController);
+//   CountFoodController.$inject = ['$scope', '$filter'];
+//
+//   function CountFoodController ($scope, $filter) {
+//     $scope.food = "";
+//     $scope.message = "";
+//     $scope.cost = .45;
+//     $scope.blah = "Cookie ";
+//     $scope.sayMessage = function () {
+//       return $filter('uppercase')("the anser is" + $filter('currency')(42, " pidor ", 28));
+//     }
+//     $scope.countFood = function () {
+//       if ($scope.food === "") {
+//          $scope.message = "Please enter data first";
+//          return;
+//       }
+//       var arrOfFood = $scope.food.split(',');
+//
+//       if (arrOfFood.length <= 3) {
+//         $scope.message = "Enjoy!";
+//       } else if (arrOfFood.length > 3) {
+//         $scope.message = "Too much!";
+//       }
+//
+//     };
+//   }
+// })();
 (function () {
-  'use sctrict';
-
   angular.module('FirstTask', [])
+  .controller('customFilterTest', customFilterTest)
+  // (2) registrate custom filter function
+  .filter('love', lovesFilter);
 
-  .controller('CountFoodController', CountFoodController);
-  CountFoodController.$inject = ['$scope'];
+  customFilterTest.$inject = ['$scope', 'lovesFilter'];//(3)add custom filter to inject ??
 
-  function CountFoodController ($scope) {
-    $scope.food = "";
-    $scope.message = "";
 
-    $scope.countFood = function () {
-      if ($scope.food === "") {
-         $scope.message = "Please enter data first";
-         return;
-      }
-      var arrOfFood = $scope.food.split(',');
-
-      if (arrOfFood.length <= 3) {
-        $scope.message = "Enjoy!";
-      } else if (arrOfFood.length > 3) {
-        $scope.message = "Too much!";
-      }
-
+  function customFilterTest($scope, lovesFilter) { // (4) don't forget about adding parameters
+    $scope.sayMessage = function () {
+      var msg = "I love JavaScript language";
+      return msg;
+    };
+    $scope.name = " hui";
+    $scope.sayLovesMessage = function () {
+      var msg = "I love JavaScript language";
+      msg = lovesFilter(msg);
+      return msg;
+    };
+  }
+  // (1) create custom filter function
+  function lovesFilter() {
+    return function (input) {
+      input = input || "";
+      input = input.replace('love', 'like');
+      return input;
     };
   }
 })();
