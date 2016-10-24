1cbfaaf88727c822e71a9cc12e6c2e82e64dc4a4
HollysMason <ibondar204@gmail.com>
2016-10-24 14:43:26 +0300

    inharitance, honestly not good understand

--- app.js
+++ app.js
@@ -1,108 +1,50 @@
-// (function () {
-//   'use sctrict';
-//
-//   angular.module('CounterApp', [])
-//   .controller('CounterController', CounterController)
-//   .controller('secondCounter', secondCounter);
-//   secondCounter.$inject = ['$scope', '$timeout'];
-//   CounterController.$inject = ['$scope'];
-//
-//   function secondCounter($scope, $timeout) {
-//     $scope.counter = 0;
-//
-//     $scope.increment = function () {
-//       // (3) verik - best varik
-//       $timeout(function () {
-//         $scope.counter++;
-//         console.log('incremented!');
-//       }, 2000);
-//       // (2) varik - norm varik no ne good
-//       // setTimeout(function () {
-//       //   $scope.$apply(function() {
-//       //     $scope.counter++;
-//       //     console.log("incremented!");
-//       //   });
-//       // (1) varik - bad varik
-//         // $scope.counter++;
-//         // console.log("incremented!");
-//         // $scope.$digest()
-//       // }, 2000);
-//     };
-//   }
-//
-//   function CounterController($scope) {
-//     $scope.onceCounter = 0;
-//     $scope.counter = 0;
-//
-//     $scope.showNumberOfWatchers = function () {
-//         console.log("# of watchers: ", $scope.$$watchersCount);
-//         console.log($scope);
-//     };
-//
-//     $scope.countOnce = function () {
-//       $scope.onceCounter = 1;
-//     };
-//
-//     $scope.upCounter = function() {
-//       $scope.counter++;
-//     };
-//
-//     // $scope.$watch('onceCounter', function (newValue, oldValue) {
-//     //   console.log("old value: ", newValue);
-//     //   console.log("new value: ", oldValue);
-//     // });
-//     //
-//     // $scope.$watch('upCounter', function (newValue, oldValue) {
-//     //   console.log("counter old value: ", newValue);
-//     //   console.log("counter new value: ", oldValue);
-//     // });
-//   }
-// })();
 (function () {
   'use strict';
 
-  var shoppingList1 = [
-     "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol",
-      "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
-  ];
+  angular.module('ControllerAsApp', [])
+  .controller('ParentController1', ParentController1)
+  .controller('ChildController1', ChildController1)
+  .controller('ParentController2', ParentController2)
+  .controller('ChildController2', ChildController2);
 
-  var shoppingList2 = [
-    {
-      name: "Milk",
-      quantity: "2"
-    },
-    {
-      name: "Donuts",
-      quantity: "200"
-    },
-    {
-      name: "Cookies",
-      quantity: "300"
-    },
-    {
-      name: "Chocolate",
-      quantity: "5"
-    }
-  ];
+  ParentController1.$inject = ['$scope'];
 
-  angular.module("ShoppingListAPP", [])
-
-  .controller("ShoppingListController", ShoppingListController);
-
-  ShoppingListController.$inject = ['$scope'];
-
-  function ShoppingListController($scope) {
-    $scope.shoppingList1 = shoppingList1;
-    $scope.shoppingList2 = shoppingList2;
-
-
-    $scope.addToList = function () {
-      var newItem = {
-        name: $scope.newItemName,
-        quantity: $scope.newItemQuantity
-      };
-
-      $scope.shoppingList2.push(newItem);
-    };
+  function ParentController1 ($scope) {
+    $scope.parentValue = 1;
+    $scope.pc = this; // this is exactly ParentController1 obj
+    $scope.pc.parentValue = 1;
   }
+
+  ChildController1.$inject = ['$scope'];
+  function ChildController1 ($scope) {
+    // console.log("$scope.parentValue: ", $scope.parentValue);
+    // console.log("CHILD $scope: ", $scope);
+    //
+    // $scope.parentValue = 5;
+    // console.log(" **CHANGED: $scope.parentValue = 5 **");
+    // console.log("$scope,parentValue: ", $scope.parentValue);
+    // console.log($scope);
+    //
+    // console.log("$scope.pc.parentValue", $scope.pc.parentValue);
+    // $scope.pc.parentValue = 5;
+    // console.log(" ** CHANGED: $scope.pc.parentValue = 5; ***");
+    // console.log("$scope.pc.parentValue: ", $scope.pc.parentValue);
+    // console.log("scope", $scope);
+  }
+
+
+  ParentController2.$inject = ['$scope'];
+  function ParentController2 () {
+    var parent = this;
+    parent.value = 1;
+  }
+
+
+  ChildController2.$inject = ['$scope'];
+  function ChildController2 ($scope) {
+    var child = this;
+    child.value = 5;
+    console.log("ChildController2 $scope: ", $scope);
+  }
+
 })()
